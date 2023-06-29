3a2a2c9fb2dfdfbd474d712a50cade93191ff592
Pablo Piazza <ppiazza@fi.uba.ar>
2023-06-28 23:21:50 -0300

    minor changes

--- src/main/java/com/example/RPG/Methods/Methods.java
+++ src/main/java/com/example/RPG/Methods/Methods.java
@@ -6,54 +6,43 @@ public class Methods {
 
 		public static void physicalDamageCalculator(Unit attacker, Unit defender) {
 
-			int attack=(int)(((attacker.getWeapon().getWAttack()+attacker.getStrength())*(1+attacker.getAbility()/100))*(Math.random()+attacker.getLuck()/100));
+			int physicalAttack=(int)(((attacker.getWeapon().getWAttack()+attacker.getStrength())*(1+attacker.getAbility()/100))*(Math.random()+attacker.getLuck()/100));
+			int physicalDefense=(int)(((defender.getArmor().getADefense()+defender.getDefence())*(1+defender.getAbility()/100))*(Math.random()+attacker.getLuck()/100));
 
-			int defense=(int)(((defender.getArmor().getADefense()+defender.getDefence())*(1+defender.getAbility()/100))*(Math.random()+attacker.getLuck()/100));
 
-
-			if(Math.random()+(attacker.getCritical()+attacker.getWeapon().getWCritical())/100>1){
-
-				attack=attack*3;
+			if(isCritical(attacker)){
+				physicalAttack=physicalAttack*3;
 			}
-
-			int damage=attack-defense;
-
+			int damage=physicalAttack-physicalDefense;
 			if(damage<0){
 				damage=0;
 			}
-
 			defender.setHp(defender.getHp()-damage);
-
 			if(defender.getHp()<=0){
-
 				defender.defeated();
 			}
 		}
 
 	public static void magicalDamageCalculator(Unit attacker, Unit defender) {
 
-		int attack=(int)(((attacker.getWeapon().getWIntelligence()+attacker.getIntelligence())*(1+attacker.getAbility()/100))*(Math.random()+attacker.getLuck()/100));
+		int magicAttack=(int)(((attacker.getWeapon().getWIntelligence()+attacker.getIntelligence())*(1+attacker.getAbility()/100))*(Math.random()+attacker.getLuck()/100));
+		int magicDefense=(int)(((defender.getArmor().getAResistance()+defender.getResistance())*(1+defender.getAbility()/100))*(Math.random()+attacker.getLuck()/100));
 
-		int defense=(int)(((defender.getArmor().getAResistance()+defender.getResistance())*(1+defender.getAbility()/100))*(Math.random()+attacker.getLuck()/100));
-
-		if(Math.random()+(attacker.getCritical()+attacker.getWeapon().getWCritical())/100>1){
-
-			attack=attack*3;
+		if(isCritical(attacker)){
+			magicAttack=magicAttack*3;
 		}
-
-		int damage=attack-defense;
-
+		int damage=magicAttack-magicDefense;
 		if(damage<0){
 			damage=0;
 		}
-
 		defender.setHp(defender.getHp()-damage);
-
 		if(defender.getHp()<=0){
-
 			defender.defeated();
 		}
 	}
+
+	private static boolean isCritical(Unit attacker) {
+		return (int) (Math.random() + (attacker.getCritical() + attacker.getWeapon().getWCritical()) / 100) > 1;}
 	}
 
 
